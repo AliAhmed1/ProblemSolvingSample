b13c95e18f28293f74b84447e59a26114bad03e8
aliahmed1 <saliahmed11111@gmail.com>
2023-05-02 00:20:03 +0500

    changes

--- flattenArray.js
+++ flattenArray.js
@@ -1,20 +1,36 @@
+// function flattenArray(arr) {
+//     let flattenedArr = [];
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       const element = arr[i];
+  
+//       if (Array.isArray(element)) {
+//         // console.log(element)
+//         flattenedArr = flattenedArr.concat(flattenArray(arr[i]));
+//       } else {
+//         // console.log()
+//         flattenedArr.push(arr[i]);
+//       }
+//     }
+  
+//     return flattenedArr;
+//   }
+
+
 function flattenArray(arr) {
-    let flattenedArr = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
-  
-      if (Array.isArray(element)) {
-        // console.log(element)
-        flattenedArr = flattenedArr.concat(flattenArray(arr[i]));
-      } else {
-        // console.log()
-        flattenedArr.push(arr[i]);
-      }
+  let result = [];
+
+  while (arr.length) {
+    let item = arr.pop();
+
+    if (Array.isArray(item)) {
+      arr.push(...item);
+    } else {
+      result.unshift(item);
     }
-  
-    return flattenedArr;
   }
-  
+
+  return result;
+}
 
   console.log(flattenArray([1,2,[3,{abc :"abc"}],[[4]]]))
